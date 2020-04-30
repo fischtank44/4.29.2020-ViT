b7f8f0c1a5b4c01bccf98bd3f51d6e115c4ac15a
fischtank44 <steven.fischbach@gmail.com>
2020-04-29 19:57:12 -0700

    This is the end of the day commit

---
+++ index.js
@@ -0,0 +1,21 @@
+console.log("Hello Stinky!!")
+    // let button = document.querySelector("#clickMe");
+
+    
+let square = document.getElementById("square");
+
+ 
+
+ 
+var button = document.getElementById("clickMe");
+console.log("button",button);
+
+button.addEventListener("click", function(){
+    square.style.borderRadius = "50%";
+    square.style.backgroundColor = "coral";
+})
+
+    // function myFunction()
+    // square.style.borderRadius = '50%'
+    // square.style.backgroundColor = 'coral'
+
